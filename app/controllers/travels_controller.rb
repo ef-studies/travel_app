13c34ee9a144eede2db78cfc0024ece687d10e39
Fernanda Andrade <fernandaao@gmail.com>
2021-06-19 11:55:40 -0300

    fiz tudo

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,3 +1,4 @@
+#TODO Api aeroportos
 class TravelsController < ApplicationController
 
   def index
@@ -6,15 +7,29 @@ class TravelsController < ApplicationController
   end
 
   def new
+    @travel 
   end
 
   def create
     response = HTTParty.post('http://localhost:3000/api/v0/travels', body: new_travel)
-    if response.code == "200"
+    if response.code == "200" || "302"
       redirect_to travels_path
     else
       redirect_to new_travel_path
     end
+  end
+
+  def edit
+    response = HTTParty.get("http://localhost:3000/api/v0/travels/#{params['id']}")
+    @travel = JSON.parse(response.body).symbolize_keys!
+  end
+
+  def update
+  end
+
+  def destroy
+    HTTParty.delete("http://localhost:3000/api/v0/travels/#{params['id']}")
+    redirect_to travels_path
   end
 
   private
